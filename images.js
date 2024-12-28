a9d870f55aa7a0852d90ee61ab26780b567f23b2
Seomgi, Han <iandmyhand@gmail.com>
2024-12-28 18:42:14 +0900

    feat: Modify more detail.

--- images.js
+++ images.js
@@ -1,7 +1,22 @@
 callback([
-  "images/dorina-perry-yQAuEZ6q2yo-unsplash.jpg",
-  "images/jonatan-pie-Lyxlh8vuRmY-unsplash.jpg",
-  "images/marie-kristin-krause-h_JyDNeuxPc-unsplash.jpg",
-  "images/pascal-debrunner-nbJyIC2tTZQ-unsplash.jpg",
-  "images/samsung-memory-QpLJ8Kw5S0Q-unsplash.jpg",
+  {
+    imageUrl: "images/dorina-perry-yQAuEZ6q2yo-unsplash.jpg",
+    link: "https://unsplash.com/photos/yQAuEZ6q2yo",
+  },
+  {
+    imageUrl: "images/jonatan-pie-Lyxlh8vuRmY-unsplash.jpg",
+    link: "https://unsplash.com/photos/Lyxlh8vuRmY",
+  },
+  {
+    imageUrl: "images/marie-kristin-krause-h_JyDNeuxPc-unsplash.jpg",
+    link: "https://unsplash.com/photos/h_JyDNeuxPc",
+  },
+  {
+    imageUrl: "images/pascal-debrunner-nbJyIC2tTZQ-unsplash.jpg",
+    link: "https://unsplash.com/photos/nbJyIC2tTZQ",
+  },
+  {
+    imageUrl: "images/samsung-memory-QpLJ8Kw5S0Q-unsplash.jpg",
+    link: "https://unsplash.com/photos/QpLJ8Kw5S0Q",
+  },
 ]);
