0c7ff42756c58a7cb3d15dba87a04ae93441c758
Seomgi, Han <iandmyhand@gmail.com>
2024-12-28 11:27:15 +0900

    feat: Add a slider that contains the images and slides them automatically.

---
+++ images.js
@@ -0,0 +1,7 @@
+callback([
+  "images/dorina-perry-yQAuEZ6q2yo-unsplash.jpg",
+  "images/jonatan-pie-Lyxlh8vuRmY-unsplash.jpg",
+  "images/marie-kristin-krause-h_JyDNeuxPc-unsplash.jpg",
+  "images/pascal-debrunner-nbJyIC2tTZQ-unsplash.jpg",
+  "images/samsung-memory-QpLJ8Kw5S0Q-unsplash.jpg",
+]);
